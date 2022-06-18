f10ced9ec0443deddc0ceeb717b13a99c6a6a2da
FiDdY_CoDeS_SoFtWaRe <98929034+FiDdY-software@users.noreply.github.com>
2022-06-18 17:41:33 -0400

    Created Your Lowered String File

---
+++ Load-Your-Lowered-String.lua
@@ -0,0 +1,5 @@
+local stringToBeLowered = "Your Word Here" --Your word goes here. Feel free to add a space.
+
+local loweredString = stringToBeLowered:lower() --Lowers the characters of your string!
+
+print(loweredString) --Finally, it prints the lowered characters, and returns them to the output!
